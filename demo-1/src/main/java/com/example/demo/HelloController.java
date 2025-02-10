d5718c2f682d228618205cbddbf511903a793637
Xizhe Li <jasperqs7@gmail.com>
2025-02-10 00:47:24 -0800

    Update Angular

--- demo-1/src/main/java/com/example/demo/HelloController.java
+++ demo-1/src/main/java/com/example/demo/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 
 public class HelloController {
-    
+
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from Spring Boot!";
