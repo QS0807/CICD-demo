70d9fd6f0a4f96a24a8ba4a35290fd5e4659b97f
Xizhe Li <jasperqs7@gmail.com>
2025-02-09 23:54:21 -0800

    Update HelloController.java

--- demo-1/src/main/java/com/example/demo/HelloController.java
+++ demo-1/src/main/java/com/example/demo/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://44.211.61.215:4200")
 @RestController
 @RequestMapping("/api")
 public class HelloController {
